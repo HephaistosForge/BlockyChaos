29f7f184594aa233851371440220fb1225978344
Robert <robert-github@posteo.de>
2023-04-15 20:41:13 +0200

    Bug Fix Immediate Win

--- entities/goal/goal.gd
+++ entities/goal/goal.gd
@@ -2,7 +2,16 @@ extends Area2D
 
 @export var type: String
 
+# Bug Fix: When Blue Player spawns, it seems that RPC call updates color
+# too late (?), there await, to give RPC call some time
+var can_detect: bool = false
+
+
+func _ready():
+	await get_tree().create_timer(1).timeout
+	can_detect = true
+
 
 func _on_body_entered(body):
-	if body.type == type:
+	if body.type == type and can_detect:
 		body.win()
