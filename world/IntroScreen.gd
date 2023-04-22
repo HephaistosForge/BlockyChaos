642107b9b33106fd36cdae1ba72f4df6e998580e
LiquidFun <brutenis@gmail.com>
2023-04-22 19:27:15 +0200

    Add intro screen as separate scene

--- world/IntroScreen.gd
+++ world/IntroScreen.gd
@@ -1,13 +1,17 @@
 extends Control
 
+#var world_scene = preload("res://world/world.tscn")
+var switching = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.visible = not Globals.did_show_intro
 
+func switch_scene():
+	if not switching:
+		switching = true
+		get_tree().change_scene_to_file("res://world/world.tscn")
 
-
-
-func _on_color_rect_gui_input(event):
-	if event is InputEventMouseButton or event.is_action_pressed("back_to_menu"):
-		visible = false
+func _input(event):
+	if not (event is InputEventMouseMotion):
+		switch_scene()
